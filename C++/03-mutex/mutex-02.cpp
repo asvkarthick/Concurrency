e3476e2fa73e379f4517f63095e3616742b6e713
Karthick Kumaran <asvkarthick@gmail.com>
2018-04-08 01:50:22 -0700

    Added lock_guard program. You don't have to unlock the mutex. It is using RAII.

---
+++ C++/03-mutex/mutex-02.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+int isDataAvailable = 0;
+std::mutex data_available_mutex;
+
+void* Producer()
+{
+    std::cout << __func__ << ": entering" << std::endl;
+
+    while(1)
+    {
+        std::cout << __func__ << ": done" << std::endl;
+
+        {
+            std::lock_guard<std::mutex> mutex(data_available_mutex);
+            isDataAvailable++;
+            std::cout << __func__ << ": data ready " << isDataAvailable << std::endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+
+    return NULL;
+}
+
+void* Consumer()
+{
+    printf("%s entering...", __func__);
+    fflush(stdout);
+    while(1)
+    {
+        std::cout << __func__ << ": done" << std::endl;
+        {
+            std::lock_guard<std::mutex> mutex(data_available_mutex);
+
+            std::cout << __func__ << ": checking if data is available." << std::endl;
+            while(isDataAvailable > 0)
+            {
+                std::cout << __func__ << ": data read " << isDataAvailable << std::endl;
+                isDataAvailable--;
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    return NULL;
+}
+
+int main(void)
+{
+    std::thread producer_thread { Producer };
+    std::thread consumer_thread { Consumer };
+
+    producer_thread.join();
+    consumer_thread.join();
+    return 0;
+}

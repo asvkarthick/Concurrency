aee456109e5e836e7e931d68959bdcb736a4ee7a
Karthick Kumaran <asvkarthick@gmail.com>
2018-08-04 18:54:12 -0700

    Added code to get value of the semaphore

---
+++ POSIX/05-semaphore/semaphore-getvalue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+    sem_t *sem;
+    int value;
+
+    sem = sem_open("karthick", 0);
+
+    sem_getvalue(sem, &value);
+    printf("Value = %d\n", value);
+
+    sem_close(sem);
+
+    return 0;
+}
